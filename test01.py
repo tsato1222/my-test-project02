addd4bcb7fca88852e6ee83f1069d6821ace25bd
tomoki.sato.jf <tomoki.sato.jf@hitachi-solutions.com>
2024-12-09 15:45:57 +0900

    test

---
+++ test01.py
@@ -0,0 +1,5 @@
+# Stripe API Key - GitHubが検出します
+STRIPE_API_KEY = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+
+def process_payment():
+    print(f"Using Stripe API key {STRIPE_API_KEY} to process payment...")
