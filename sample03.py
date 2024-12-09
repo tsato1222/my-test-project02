7409c2f45414b34f806b398d18f153664069ba89
tomoki.sato.jf <tomoki.sato.jf@hitachi-solutions.com>
2024-12-09 15:44:02 +0900

    test

---
+++ sample03.py
@@ -0,0 +1,8 @@
+# GitHub Personal Access Token (PAT)
+# WARNING: Do NOT include sensitive keys in your code like this in production!
+github_personal_access_token = 'ghp_1234567890abcdef1234567890abcdef12345678'
+
+def fetch_github_repositories():
+    # Pretend to fetch GitHub repositories using the GITHUB_TOKEN
+    print(f"Using token {github_personal_access_token} to fetch repositories...")
+    # Some logic to fetch repositories would go here...
